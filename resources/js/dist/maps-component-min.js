b7b304bd46ed048419f7331adde90bddbda457e6
asaonline <asaonline1@gmail.com>
2019-02-09 21:28:40 +0530

    sd

--- resources/js/dist/maps-component-min.js
+++ resources/js/dist/maps-component-min.js
@@ -7,9 +7,22 @@ $(".test" ).hover(
 
   $( document ).ready(function() {
     console.log( "ready!" );
+    $("#t" ).hover(
+        function() {
+          console.log("print");
+        }
+      );
+      
 });
 
 
 $( ".test" ).click(function() {
     alert( "Handler for .click() called." );
   });
+
+
+  $("#t" ).hover(
+    function() {
+      console.log("print");
+    }
+  );
