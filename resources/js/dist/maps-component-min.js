0855157d4801a8862216af31e0e87b01cdcaec70
asaonline <asaonline1@gmail.com>
2019-02-10 01:12:32 +0530

    ho

--- resources/js/dist/maps-component-min.js
+++ resources/js/dist/maps-component-min.js
@@ -12,13 +12,17 @@
 
   
 
+    var fbButton = document.getElementById('fb-share-button');
+    var url = window.location.href;
+    
+    fbButton.addEventListener('click', function() {
+        window.open('https://www.facebook.com/sharer/sharer.php?u=' + url,
+            'facebook-share-dialog',
+            'width=800,height=600'
+        );
+        return false;
+    });
 
-    $(".ui.facebook.button").click(function() {
-        FB.ui({
-            method: 'share',
-            href: x,
-}, function(response){});
-    })
 
 });
 
